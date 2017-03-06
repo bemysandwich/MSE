3f10b305ce3e0b070fd96df20f326616627b22a4
bemysandwich <kai.eschenbach@gmx.de>
2017-03-06 09:10:23 +0100

    test

--- WindowsFormsAppGitHubTest/Form1.Designer.cs
+++ WindowsFormsAppGitHubTest/Form1.Designer.cs
@@ -44,7 +44,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(243, 9);
+            this.label1.Location = new System.Drawing.Point(243, 33);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(29, 13);
             this.label1.TabIndex = 1;
